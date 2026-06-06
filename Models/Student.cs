32a6c03ce88859eec4456d90f60e2acb155271bd
TanKhanhabc <khanhphamabcht@gmail.com>
2026-06-06 15:29:15 +0700

    Commit2

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,5 +5,8 @@ namespace DapperApi.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Age { get; set; }
+        
+        // Thêm Email
+        public string Email { get; set; } = string.Empty;
     }
 }
